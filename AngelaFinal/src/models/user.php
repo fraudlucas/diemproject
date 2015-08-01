e608c1ceaeff826988ecabff13a7554325c97436
HugoMagalhaess <hugo.magalhaess@gmail.com>
2015-08-01 14:06:22 -0400

    test no user para commit

--- AngelaFinal/src/models/user.php
+++ AngelaFinal/src/models/user.php
@@ -15,6 +15,7 @@ class User{
     private $province;
     private $administratorID;
 	private $status;
+    private $hugoteestgit;  //test git
 	
     function getUserName() {
         return $this->userName;
