15fc66bcc7e1046a3ce8f997c4ce9757aee36638
mperezch <mperezch@users.noreply.github.com>
2015-08-06 18:10:06 -0400

    Update headerAdmin.php

--- AngelaFinal/web/layout/headerAdmin.php
+++ AngelaFinal/web/layout/headerAdmin.php
@@ -17,8 +17,9 @@
 					
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="index.php">Home</a></li>
+						<li><a href="../../web/pages/adminHome.php">Profile</a></li>
                         <li><a href="http://projectblog.byethost12.com/wp/" >Blog</a></li>
-                        <li><a href="web/pages/contact.php" >Contact</a></li>
+                        <li><a href="../../web/pages/contact.php" >Contact</a></li>
 						<li>
 							<form class="navbar-form"  action="src/handlers/userHandler.php?a=logout" method="post">
 								<div class="form-group">
