6d6559c54cb1a87fabc7c946e3bbc1856e9b0cd0
khsk <eatparalytic@gmail.com>
2015-08-06 11:44:39 +0900

    fix var name $clothingLookPieces to $clothingLookPiecesList

--- AngelaFinal/src/controllers/clothingLookPiecesController.php
+++ AngelaFinal/src/controllers/clothingLookPiecesController.php
@@ -28,7 +28,7 @@ class ClothingLookPiecesController {
 		
 		$clothingLookPiecesList = $this->persistenceClothes->listAll();
 
-		return $clothingLookPieces;
+		return $clothingLookPiecesList;
 	}
 
 }
