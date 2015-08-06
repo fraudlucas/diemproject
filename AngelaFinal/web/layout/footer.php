f4259b12cb3157b2b32997b97ba688e3ff5c350a
mperezch <mperezch@users.noreply.github.com>
2015-08-06 13:22:25 -0400

    Update footer.php
    Final version for footer.

--- AngelaFinal/web/layout/footer.php
+++ AngelaFinal/web/layout/footer.php
@@ -9,7 +9,6 @@
                             <a href="./web/pages/aboutUs.php">About Us</a>
                         <br>
                             <a href="./web/pages/ourTeam.php">Our team</a>
-                        
                         <br>
                             <a href="./web/pages/testimonials.php">Testimonials</a>
                         <br>
@@ -23,15 +22,27 @@
 				  <h4 style= "text-transform: uppercase; text-align: center; text-color:#000000;">Contact Info</h4>
                 <address>
                     <p class="text-center">
-                        <i class="fa fa-home pr-10"></i>Address: 231, King Street</p>
+                        <i class="fa fa-home pr-10">&nbsp&nbsp</i>Address: 231, King Street</p>
                     <p class="text-center">
-                        <i class="fa fa-globe pr-10"></i>Peterborough, Ontario / Canada</p>
+                        <i class="fa fa-globe pr-10">&nbsp&nbsp</i>Peterborough, Ontario / Canada</p>
                     <p class="text-center">
-                        <i class="fa fa-phone pr-10"></i>Phone: 705.742.7315</p>
+                        <i class="fa fa-phone pr-10">&nbsp&nbsp</i>Phone: 705.742.7315</p>
                     <p class="text-center">
-                        <i class="fa fa-envelope pr-10"></i>Email: showroom@angelamark.com</p>
+                        <i class="fa fa-envelope pr-10">&nbsp&nbsp</i>Email: showroom@angelamark.com</p>
                 </address>
-			<div class="social">
+			
+				  </div>
+                  <div class="col-md-4">
+				  
+                    <ul class="page-footer-list text-center">
+					<h4 style= " text-transform: uppercase; text-center; text-colour:#000000;">Client Area</h4>
+                        
+                            <a href="#" data-toggle="modal" data-target="#myBook">Book an appointment</a>
+                        <br>
+                            <a href="./web/pages/userWardrobe.php">My Wardrobe</a>
+                        <br>
+                   
+					<div class="social">
 			<h4 style= "text-transform: uppercase; text-align: center; text-color:#000000;">Social media</h4>
     <center><ul>
         <li><a href="https://www.facebook.com/angelamarkfashion?fref=ts"><i class="fa fa-lg fa-facebook"></i></a></li>
@@ -61,22 +72,9 @@ padding: 1px;
     list-style: none outside none;
     display: inline-block;
 } </style>
+ </ul>
+				
 				  </div>
-				  </div>
-                  <div class="col-md-4">
-				  
-                    <ul class="page-footer-list text-center">
-					<h4 style= " text-transform: uppercase; text-center; text-colour:#000000;">Client Area</h4>
-                        
-                            <a href="#" data-toggle="modal" data-target="#myBook">Book an appointment</a>
-                        <br>
-                            <a href="#" data-toggle="modal" data-target="#loginmodal">Log in</a>
-                        <br>
-                            <a href="#" data-toggle="modal" data-target="#myModal">Register</a>
-                        <br>
-                            <a href="./web/pages/userWardrobe.php">My Wardrobe</a>
-                        <br>
-                    </ul>
 				  </div>
                 </div>
               </div>
