b830e6eaaee5e74f5a27321e171bbf0cedf0f9df
mperezch <mperezch@users.noreply.github.com>
2015-08-06 16:33:58 -0400

    Update testimonials.php
    Change breadcrumb on testimonials.

--- AngelaFinal/web/pages/testimonials.php
+++ AngelaFinal/web/pages/testimonials.php
@@ -9,15 +9,38 @@
 	$managementContent = $managementContentView->searchContent('pageId','2','1');
 	$session = new Session();
 ?>
-
 <html lang="en">
 <head>
 	<?php include( DIR_LAY.'headPages.php');?>
+	<style>
+div.wrapper{  
+    float:left; /* important */  
+    position:relative; /* important(so we can absolutely position the description div */ 
+	
+}  
+div.description{  
+    position:absolute; /* absolute position (so we can position it where we want)*/  
+    bottombottom:0px; /* position will be on bottom */  
+    left:0px;  
+    width:100%;  
+    /* styling bellow */  
+    background-color:black;  
+    font-family: 'tahoma';  
+    font-size:15px;  
+    color:white;  
+    opacity:0.6; /* transparency */  
+    filter:alpha(opacity=60); /* IE transparency */  
+}  
+p.description_content{  
+    padding:10px;  
+    margin:0px;  
+}  
+</style>
 </head>
 <body>
 <?php include( DIR_LAY.'modalBook.php');?>
 	<div id="wrapper">
-		<?php 
+	<?php 
 		if($session->isLoggedIn()){
 			if ($_SESSION['role']==2){
 				include (DIR_LAY.'headerUserPages.php') ;
@@ -28,30 +51,37 @@
 			include( DIR_LAY.'headerPages.php') ;
 		}
 		?>
-		<section id="inner-headline">
-		<div class="container" style="margin-top:100px">
-			<div class="row">
-				<div class="col-lg-12">
-					<ul class="breadcrumb">
-						<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
-						<li><a href="#">About</a><i class="icon-angle-right"></i></li>
-						<li class="active">Testimonials</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		</section>
-		<!--
-		<section id="featured">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-										
-					</div>					
-				</div>
-			</div>			
-		</section>	 -->
-		
+?>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+     
+        <!--breadcrumbs start-->
+        <div class="breadcrumbs">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-sm-4">
+                        <h1></h1>
+                    </div>
+                    <div class="col-lg-8 col-sm-8">
+                        <ol class="breadcrumb pull-right">
+                            <li>
+                                <a href="../../index.php">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li class="./web/pages/testimonials.php">Testimonials</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
 		<!-- Slider has been changed -->
 		<div class="container">
             <div class="row mar-b-50">
@@ -61,19 +91,10 @@
                         <div class="carousel-inner">
                             <div class="item active">
                                 <!-- <img src="images/url.jpg"> -->
-								<?php include( DIR_LAY.'slider2.php') ?>
-                                <div class="carousel-caption">
-                                    <h2>Kathy</h2>
-                                    <p>“Angela,  Just wanted to thank you for my beautiful suit, the wonderful service & an amazing shopping experience.”
-									</p>
-                                </div>
+								<?php include( DIR_LAY.'slider2.php') ?>                               
                             </div>
 						<div class="item">
-                                <?php include( DIR_LAY.'slider2.php') ?>
-                                <div class="carousel-caption">
-                                    <h2>Rhonda Barnet</h2>
-                                    <p> Sent us this image of her looking stunning and professional in her Angela Mark creation!</p>
-                                </div>
+                                <?php include( DIR_LAY.'slider2.php') ?>                               
                             </div>
                         </div>
                         <a class="left carousel-control" href="#carousel-example" data-slide="prev"><i class="icon-prev  fa fa-angle-left"></i></a>
