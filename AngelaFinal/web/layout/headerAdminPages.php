82a0dbcc6efebe29334fbe02bf0cab71680c3936
mperezch <chaves.manoela@outlook.com>
2015-08-07 16:05:47 -0400

    Fix broken link son Admin

--- AngelaFinal/web/layout/headerAdminPages.php
+++ AngelaFinal/web/layout/headerAdminPages.php
@@ -9,17 +9,17 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html">
-						<img src="../assets/images/logo2.png">
+                    <a class="navbar-brand" href="../../index.php">
+						<img src="../../web/assets/images/logo2.png">
 					</a>
                 </div>
                 <div class="navbar-collapse collapse " id="navbar-main">
 					
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="../../index.php">Home</a></li>
-						<li><a href="../../web/pages/adminHome.php">Profile</a></li>
+						<li><a href="../pages/adminHome.php">Profile</a></li>
                         <li><a href="http://projectblog.byethost12.com/wp/" >Blog</a></li>
-                        <li><a href="../../web/pages/contact.php" >Contact</a></li>
+                        <li><a href="../pages/contact.php" >Contact</a></li>
 						<li>
 							<form class="navbar-form"  action="../../src/handlers/userHandler.php?a=logout" method="post">
 								<div class="form-group">
